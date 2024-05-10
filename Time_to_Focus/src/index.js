7171306928f7b454e6ae84ce468b30cfac40fd77
GuilhLopes <guilherme.marqueslopes@edu.unifil.br>
2024-05-10 20:37:33 -0300

    Adicionando o contador

--- Time_to_Focus/src/index.js
+++ Time_to_Focus/src/index.js
@@ -1,19 +1,44 @@
 const express = require('express');
 const session = require('express-session');
-const bodyparser = require('body-parser');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const sessao = session({
+    secret: '2C44-4D44-WppQ38S',
+    resave: true,
+    saveUninitialized: true
+});
 
 const port = 3000;
 var path = require('path');
-const app = express();
 
-app.use(session({secret:'2h1vbjwbqndj2b1i4k1n4'}));
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(bodyparser.json());
+app.use(sessao);
+
+// Middleware para inicialização de sessão no Socket.IO
+io.use((socket, next) => {
+    sessao(socket.request, {}, next);
+});
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', path.join(__dirname, '../views'));
 
+let totalVisitas = 0;
+
+// Middleware para controlar a primeira visita à rota /
+app.use((req, res, next) => {
+    if (!req.session.visitouPaginaInicial && req.path === '/') {
+        req.session.visitouPaginaInicial = true;
+        totalVisitas++;
+        req.session.visitas = totalVisitas;
+        io.emit('visita', totalVisitas);
+    }
+    next();
+});
 
 app.get('/', function(req,res){
     res.render('inicial');
@@ -26,6 +51,13 @@ app.get('/pomodoro', function(req,res){
 app.get('/feynman', function(req,res){
     res.render('metodoFeynman');
 });
-app.listen(port, ()=>{
+
+server.listen(port, ()=>{
     console.log('Site está rodando na porta 3000!');
+});
+
+io.on('connection', function(socket){
+    if (socket.request.session.visitouPaginaInicial) {
+        socket.emit('visita', totalVisitas);
+    }
 });
